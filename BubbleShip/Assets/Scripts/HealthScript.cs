b4fe44b62cabb4ce4c2db47a716fa5976eccb732
Marco <marco.latona92@gmail.com>
2015-11-03 11:03:26 +0100

    Solved some errors

--- BubbleShip/Assets/Scripts/HealthScript.cs
+++ BubbleShip/Assets/Scripts/HealthScript.cs
@@ -5,26 +5,38 @@ public class HealthScript : MonoBehaviour {
 
 	public int hp = 3;
 
+
 	// Method called when there is a collision between a bubble and the spaceship
 	void OnTriggerEnter2D (Collider2D collider){
 
 		Debug.Log("collision");
 
+
 		Bubble bubble = collider.gameObject.GetComponent<Bubble> ();
 
+		Debug.Log("Bubble Damage: "+ bubble.damage);
+
 		hp -= bubble.damage;
+
+		Debug.Log("Lifes left: "+ hp);
 
 		Destroy (bubble.gameObject);
 
 		if (hp == 2) {
+			Debug.Log("hp==2");
+
 			Destroy(GameObject.FindGameObjectWithTag("HP1"));
 		}
 
 		if (hp == 1) {
+			Debug.Log("hp==1");
+
 			Destroy(GameObject.FindGameObjectWithTag("HP2"));
 		}
 
 		if (hp <= 0) {
+			Debug.Log("hp==0");
+
 			Destroy(GameObject.FindGameObjectWithTag("HP3"));
 			Destroy (gameObject);
 		}
