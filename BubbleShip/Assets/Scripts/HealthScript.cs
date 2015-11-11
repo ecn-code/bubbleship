78d55c25e6d87cee4634f00146e9f733f739bfe4
elcanie <elcanie@inf.upv.es>
2015-11-11 17:29:01 +0100

    Bug on bubble collision
    When bubble collides with other bubble and is colliding with the
    spaceship

--- BubbleShip/Assets/Scripts/HealthScript.cs
+++ BubbleShip/Assets/Scripts/HealthScript.cs
@@ -17,46 +17,48 @@ public class HealthScript : MonoBehaviour {
 
 	// Method called when there is a collision between a bubble and the spaceship
 	void OnTriggerEnter2D (Collider2D collider){
+		handleCollision (collider);
+	}
 
+	void OnTriggerStay2D(Collider2D collider){
+		handleCollision (collider);
+	}
 
-
-
+	void handleCollision(Collider2D collider){
 		Bubble bubble = collider.gameObject.GetComponent<Bubble> ();
-
-
+		
+		
 		if (bubble.playerFired) {
 			return;
 		}
-
+		
 		Debug.Log("collision");
 		Debug.Log("Bubble Damage: "+ bubble.damage);
-
+		
 		hp -= bubble.damage;
-
+		
 		Debug.Log("Lifes left: "+ hp);
-
+		
 		//Destroy (bubble.gameObject);
 		gameController.destroy (bubble.gameObject);
-
+		
 		if (hp == 2) {
 			Debug.Log("hp==2");
 			GameObject.FindGameObjectWithTag("HP1").GetComponent<Image> ().sprite = hpRemoved;
 			//Destroy(GameObject.FindGameObjectWithTag("HP1"));
 		}
-
+		
 		if (hp == 1) {
 			Debug.Log("hp==1");
 			GameObject.FindGameObjectWithTag("HP2").GetComponent<Image> ().sprite = hpRemoved;
 			//Destroy(GameObject.FindGameObjectWithTag("HP2"));
 		}
-
+		
 		if (hp <= 0) {
 			Debug.Log("hp==0");
 			GameObject.FindGameObjectWithTag("HP3").GetComponent<Image> ().sprite = hpRemoved;
 			//Destroy(GameObject.FindGameObjectWithTag("HP3"));
 			Destroy (gameObject);
 		}
-
 	}
-
 }
