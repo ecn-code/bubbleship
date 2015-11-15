ac08470ddaad5e6e717f5d3341bb92fec693a0ad
Marco <marco.latona92@gmail.com>
2015-11-15 22:32:31 +0100

    Solved each bug collision

--- BubbleShip/Assets/Scripts/Collision.cs
+++ BubbleShip/Assets/Scripts/Collision.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Collision : MonoBehaviour {
-
+	
 	public int hp = 3;
 	GameController gameController;
 	public Sprite hpRemoved;
@@ -12,66 +12,66 @@ public class Collision : MonoBehaviour {
 	{
 		
 		gameController = GameController.Instance ();
+
 	}
 	
 	
-	// Method called when there is a collision between a bubble and the spaceship
-	void OnTriggerEnter2D (Collider2D collider){
+	// Methods called when there is a collision between a walls and the spaceship
+	void OnCollisionEnter2D (Collision2D col){
+		handleCollision (col);
+	}
+	
+	void OnCollisionStay2D(Collision2D col){
+		handleCollision (col);
+	}
+	
+	void handleCollision(Collision2D col){
 		
-		if (collider.name == "Wall") {
+		//Debug.Log (col.gameObject.name);
+		
+		if (col.gameObject.name == "RightWall" || col.gameObject.name == "LeftWall") {
 
-			Debug.Log (collider.name);
-
-
-		} else {
-
-			Debug.Log (collider.name);
-			
-			Bubble bubble = collider.gameObject.GetComponent<Bubble> ();
-			
-			
-			if (bubble.playerFired) {
-				return;
-			}
-			
-			Debug.Log ("collision");
-			Debug.Log ("Bubble Damage: " + bubble.damage);
-			
-			hp -= bubble.damage;
-			
-			Debug.Log ("Lifes left: " + hp);
-			
-			//Destroy (bubble.gameObject);
-			gameController.destroy (bubble.gameObject);
-			
-			if (hp == 2) {
-				Debug.Log ("hp==2");
-				GameObject.FindGameObjectWithTag ("HP1").GetComponent<Image> ().sprite = hpRemoved;
-				//Destroy(GameObject.FindGameObjectWithTag("HP1"));
-			}
-			
-			if (hp == 1) {
-				Debug.Log ("hp==1");
-				GameObject.FindGameObjectWithTag ("HP2").GetComponent<Image> ().sprite = hpRemoved;
-				//Destroy(GameObject.FindGameObjectWithTag("HP2"));
-			}
-			
-			if (hp <= 0) {
-				Debug.Log ("hp==0");
-				GameObject.FindGameObjectWithTag ("HP3").GetComponent<Image> ().sprite = hpRemoved;
-				//Destroy(GameObject.FindGameObjectWithTag("HP3"));
-				Destroy (gameObject);
-			}
+			return;
 		}
-	} 
-
-	// Use this for initialization
-	void Start () {
-	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	// Methods called when there is a collision between the bubbles and the spaceship
+	void OnTriggerEnter2D(Collider2D col){
+
+		Bubble bubble = col.gameObject.GetComponent<Bubble>();
+			
+			
+		if (bubble.playerFired) {
+				return;
+		}
+			
+		Debug.Log ("collision");
+		Debug.Log ("Bubble Damage: " + bubble.damage);
+			
+		hp -= bubble.damage;
+			
+		Debug.Log ("Lifes left: " + hp);
+			
+		//Destroy (bubble.gameObject);
+		gameController.destroy (bubble.gameObject);
+			
+		if (hp == 2) {
+			Debug.Log ("hp==2");
+			GameObject.FindGameObjectWithTag ("HP1").GetComponent<Image> ().sprite = hpRemoved;
+			//Destroy(GameObject.FindGameObjectWithTag("HP1"));
+		}
+			
+		if (hp == 1) {
+			Debug.Log ("hp==1");
+			GameObject.FindGameObjectWithTag ("HP2").GetComponent<Image> ().sprite = hpRemoved;
+			//Destroy(GameObject.FindGameObjectWithTag("HP2"));
+		}
+			
+		if (hp <= 0) {
+			Debug.Log ("hp==0");
+			GameObject.FindGameObjectWithTag ("HP3").GetComponent<Image> ().sprite = hpRemoved;
+			//Destroy(GameObject.FindGameObjectWithTag("HP3"));
+			Destroy (gameObject);
+		}
 	}
 }
--- BubbleShip/Assets/Scripts/HealthScript.cs
+++ BubbleShip/Assets/Scripts/HealthScript.cs
@@ -10,64 +10,64 @@ public class HealthScript : MonoBehaviour {
 
 	void Awake ()
 	{
-
 		gameController = GameController.Instance ();
 	}
 
 
-	// Method called when there is a collision between a bubble and the spaceship
-	void OnTriggerEnter2D (Collider2D collider){
-
-		handleCollision (collider);
+	// Methods called when there is a collision between the wall and the spaceship
+	void OnCollisionEnter2D (Collision2D col){
+		handleCollision (col);
 	}
 
-	void OnTriggerStay2D(Collider2D collider){
-		handleCollision (collider);
+	void OnCollisionStay2D(Collision2D col){
+		handleCollision (col);
 	}
 
-	void handleCollision(Collider2D collider){
-	
-		if (collider.name == "Wall") {
-			return;
-		}
+	void handleCollision(Collision2D col){
 
-		Debug.Log (collider.name);
+		Debug.Log (col.gameObject.name);
 	
+		if (col.gameObject.name == "RightWall" || col.gameObject.name == "LeftWall") {
 
-		Bubble bubble = collider.gameObject.GetComponent<Bubble> ();
-		
-		
-		if (bubble.playerFired) {
 			return;
-		}
-		
-		Debug.Log("collision");
-		Debug.Log("Bubble Damage: "+ bubble.damage);
-		
-		hp -= bubble.damage;
-		
-		Debug.Log("Lifes left: "+ hp);
-		
-		//Destroy (bubble.gameObject);
-		gameController.destroy (bubble.gameObject);
-		
-		if (hp == 2) {
-			Debug.Log("hp==2");
-			GameObject.FindGameObjectWithTag("HP1").GetComponent<Image> ().sprite = hpRemoved;
-			//Destroy(GameObject.FindGameObjectWithTag("HP1"));
-		}
-		
-		if (hp == 1) {
-			Debug.Log("hp==1");
-			GameObject.FindGameObjectWithTag("HP2").GetComponent<Image> ().sprite = hpRemoved;
-			//Destroy(GameObject.FindGameObjectWithTag("HP2"));
-		}
-		
-		if (hp <= 0) {
-			Debug.Log("hp==0");
-			GameObject.FindGameObjectWithTag("HP3").GetComponent<Image> ().sprite = hpRemoved;
-			//Destroy(GameObject.FindGameObjectWithTag("HP3"));
-			Destroy (gameObject);
+
+		} else {
+
+			Bubble bubble = col.gameObject.GetComponent<Bubble>();
+			
+			
+			if (bubble.playerFired) {
+				return;
+			}
+			
+			Debug.Log ("collision");
+			Debug.Log ("Bubble Damage: " + bubble.damage);
+			
+			hp -= bubble.damage;
+			
+			Debug.Log ("Lifes left: " + hp);
+			
+			//Destroy (bubble.gameObject);
+			gameController.destroy (bubble.gameObject);
+			
+			if (hp == 2) {
+				Debug.Log ("hp==2");
+				GameObject.FindGameObjectWithTag ("HP1").GetComponent<Image> ().sprite = hpRemoved;
+				//Destroy(GameObject.FindGameObjectWithTag("HP1"));
+			}
+			
+			if (hp == 1) {
+				Debug.Log ("hp==1");
+				GameObject.FindGameObjectWithTag ("HP2").GetComponent<Image> ().sprite = hpRemoved;
+				//Destroy(GameObject.FindGameObjectWithTag("HP2"));
+			}
+			
+			if (hp <= 0) {
+				Debug.Log ("hp==0");
+				GameObject.FindGameObjectWithTag ("HP3").GetComponent<Image> ().sprite = hpRemoved;
+				//Destroy(GameObject.FindGameObjectWithTag("HP3"));
+				Destroy (gameObject);
+			}
 		}
 	}
 }
